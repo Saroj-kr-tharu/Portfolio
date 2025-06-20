7cd1dc6139f1236fafcc04b7ac4a8b4948ffd713
Saroj-kr-tharu <sarojkumartharu11234@gmail.com>
2025-06-20 18:33:45 +0545

    fix hero section

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -80,7 +80,7 @@ function HeroSection(){
                     viewport={{ once: true }}
 
                         onClick={handleResumeDownload}
-                        className="flex items-center  justify-center md:justify-start px-0 sm:px-4 md:px-6 py-2 md:py-3 border-1 border-white rounded-lg hover:cursor-pointer hover:bg-white hover:border-indigo-900 hover:text-black
+                        className="flex items-center  justify-center md:justify-start px-0 sm:px-4 md:px-6 py-2 md:py-3 border-1 border-base-400 rounded-lg hover:cursor-pointer hover:bg-white hover:border-indigo-900 hover:text-black
                          transition-all duration-300 ease-in-out transform hover:scale-105 w-full md:w-auto"
                     >
                         <span className="mr-2 text-xl md:text-2xl"><IoMdDownload /></span> Download Resume
@@ -95,7 +95,7 @@ function HeroSection(){
                     }}
                     viewport={{ once: true }}
                         onClick={handleContactMe}
-                        className="flex items-center justify-center md:justify-start px-0 sm:px-4 md:px-6 py-2 md:py-3 bg-transparent border border-white hover:cursor-pointer hover:bg-white hover:border-indigo-900 hover:text-black rounded-lg transition-all duration-300 ease-in-out transform hover:scale-105 w-full md:w-auto"
+                        className="flex items-center justify-center md:justify-start px-0 sm:px-4 md:px-6 py-2 md:py-3 bg-transparent border-1 border-base-400 hover:cursor-pointer hover:bg-white hover:border-indigo-900 hover:text-black rounded-lg transition-all duration-300 ease-in-out transform hover:scale-105 w-full md:w-auto"
                     >
                         <span className="mr-2 text-lg md:text-xl"><FiMessageCircle /></span> Chat with me
                     </motion.button>
